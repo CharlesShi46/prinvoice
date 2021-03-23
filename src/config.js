e86a517dbbe6c4b207de6f1842bbd4fe8180a31c
Charles Shi <charlesshi@Charless-MacBook-Air.local>
2021-03-23 07:31:31 -0500

    Anonymize userbase ID

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'prinvoice'
 export const DEFAULT_CURRENCY = 'USD'
 
-export const USERBASE_APP_ID = '10867920-7791-4871-abba-33483ba37c87'
+export const USERBASE_APP_ID = 'USERBASE_ID'
 export const USERBASE_DATABASE_NAME = 'prinvoice_main'
